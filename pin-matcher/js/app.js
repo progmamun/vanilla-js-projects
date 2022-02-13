9d4a9ffe4cc0b4cb337e5db4aacdfec87ce343e1
progmamun <mamun333089@gmail.com>
2022-02-13 17:55:01 +0600

    random-num-generate

---
+++ pin-matcher/js/app.js
@@ -0,0 +1,14 @@
+function getPin() {
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + '';
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    // console.log('Got 2 digit and calling again', pin);
+    return getPin();
+  }
+}
+function generatePin() {
+  const pin = getPin();
+  document.getElementById('display-pin').value = pin;
+}
