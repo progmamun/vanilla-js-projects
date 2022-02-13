f41a99a66d304c2fca9497ca8fcc2057b1fa5973
progmamun <mamun333089@gmail.com>
2022-02-13 18:34:19 +0600

    calc

--- pin-matcher/js/app.js
+++ pin-matcher/js/app.js
@@ -12,3 +12,18 @@ function generatePin() {
   const pin = getPin();
   document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+  const number = event.target.innerText;
+  const calcInput = document.getElementById('typed-numbers');
+
+  if (isNaN(number)) {
+    if (number == 'C') {
+      calcInput.value = '';
+    }
+  } else {
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+});
